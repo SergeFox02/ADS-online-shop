e56443a2d5fc61a6e318b9b86fa64766e43eb4b9
SergeFox02 <Fox2188@gmail.com>
2022-09-03 19:14:08 +0300

    Create AdsController and Create basic methods

---
+++ src/main/java/ru/skypro/homework/controller/AdsController.java
@@ -0,0 +1,65 @@
+package ru.skypro.homework.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/ads")
+public class AdsController {
+
+    @GetMapping
+    public ResponseEntity<?> getAllAds(){
+        return ResponseEntity.ok("Get All ads");
+    }
+
+    @PostMapping
+    public ResponseEntity<?> addAds(){
+        return ResponseEntity.ok("Add new ads");
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<?> getAdsMe(){
+        return ResponseEntity.ok("Get Ads me");
+    }
+
+    @GetMapping("/{ad_pk}/comment")
+    public ResponseEntity<?> getAdsComments(@PathVariable Long ad_pk){
+        return ResponseEntity.ok("Get Ads comments pk = " + ad_pk);
+    }
+
+    @PostMapping("/{ad_pk}/comment")
+    public ResponseEntity<?> addAdsComment(@PathVariable Long ad_pk){
+        return ResponseEntity.ok("Add Ads comment pk = " + ad_pk);
+    }
+
+    @DeleteMapping("/{ad_pk}/comment/{id}")
+    public ResponseEntity<?> deleteAdsComment(@PathVariable Long ad_pk, @PathVariable Long id){
+        return ResponseEntity.ok("Delete Ads comment pk = " + ad_pk + "; comment id = " + id);
+    }
+
+    @GetMapping("/{ad_pk}/comment/{id}")
+    public ResponseEntity<?> getAdsComment(@PathVariable Long ad_pk, @PathVariable Long id){
+        return ResponseEntity.ok("Get Ads comment pk = " + ad_pk + "; comment id = " + id);
+    }
+
+    @PatchMapping("/{ad_pk}/comment/{id}")
+    public ResponseEntity<?> updateAdsComment(@PathVariable Long ad_pk, @PathVariable Long id){
+        return ResponseEntity.ok("Update Ads comment pk = " + ad_pk + "; comment id = " + id);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> removeAds(@PathVariable Long id){
+        return ResponseEntity.ok("Remove Ads id = " + id);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getAds(@PathVariable Long id){
+        return ResponseEntity.ok("Get Ads id = " + id);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<?> updateAds(@PathVariable Long id){
+        return ResponseEntity.ok("Update Ads id = " + id);
+    }
+
+}
