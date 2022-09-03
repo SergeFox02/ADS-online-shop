66cc31a23c508d178af28e375f8f7aacc388847d
SergeFox02 <Fox2188@gmail.com>
2022-09-03 18:53:10 +0300

    Create UserController and Create basic methods

---
+++ src/main/java/ru/skypro/homework/controller/UserController.java
@@ -0,0 +1,34 @@
+package ru.skypro.homework.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @PostMapping
+    public ResponseEntity<?> addUser(){
+        return ResponseEntity.ok("Add users");
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<?> getUsers(){
+        return ResponseEntity.ok("Get users");
+    }
+
+    @PatchMapping("/me")
+    public ResponseEntity<?> updateUser(){
+        return ResponseEntity.ok("Update user");
+    }
+
+    @PostMapping("/set_password")
+    public ResponseEntity<?> setPassword(){
+        return ResponseEntity.ok("Set password of user");
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getUser(@PathVariable Long id){
+        return ResponseEntity.ok("Get users with id = " + id);
+    }
+}
