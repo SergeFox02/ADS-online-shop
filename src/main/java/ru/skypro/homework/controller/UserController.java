534b5da4f409f168f82031cf7eaecb6ff1e03c62
SergeFox02 <Fox2188@gmail.com>
2022-09-27 19:19:43 +0300

    Добавил ApiDoc в UserController.java

--- src/main/java/ru/skypro/homework/controller/UserController.java
+++ src/main/java/ru/skypro/homework/controller/UserController.java
@@ -1,33 +1,212 @@
 package ru.skypro.homework.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import ru.skypro.homework.Model.User;
 
 @CrossOrigin(value = "http://localhost:3000")
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
-    @PostMapping
-    public ResponseEntity<?> addUser(){
-        return ResponseEntity.ok("Add users");
-    }
+    private final String TAG_USER_CONTROLLER = "Пользователи";
 
+    @Operation(
+            summary = "updateUser",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "OK",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = User.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "204",
+                            description = "No content",
+                            content = @Content(mediaType = MediaType.TEXT_PLAIN_VALUE)
+                    ),
+                    @ApiResponse(
+                            responseCode = "403",
+                            description = "Forbidden",
+                            content = @Content(mediaType = MediaType.TEXT_PLAIN_VALUE)
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Not Found",
+                            content = @Content(mediaType = MediaType.TEXT_PLAIN_VALUE)
+                    )
+            },
+            tags = TAG_USER_CONTROLLER
+    )
     @GetMapping("/me")
     public ResponseEntity<?> getUsers(){
         return ResponseEntity.ok("Get users");
     }
 
+    @Operation(
+            summary = "addUser",
+            description = "user",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = User.class)
+                    )
+            ),
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "OK",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = User.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "204",
+                            description = "No Content",
+                            content = @Content(mediaType = MediaType.TEXT_PLAIN_VALUE)
+                    ),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Unauthorized",
+                            content = @Content(mediaType = MediaType.TEXT_PLAIN_VALUE)
+                    ),
+                    @ApiResponse(
+                            responseCode = "403",
+                            description = "Forbidden",
+                            content = @Content(mediaType = MediaType.TEXT_PLAIN_VALUE)
+                    )
+            },
+            tags = TAG_USER_CONTROLLER
+    )
+    @PostMapping
+    public ResponseEntity<?> addUser(){
+        return ResponseEntity.ok("Add users");
+    }
+
+    @Operation(
+            summary = "updateUser",
+            description = "user",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = User.class)
+                    )
+            ),
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "OK",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = User.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "204",
+                            description = "No Content",
+                            content = @Content(mediaType = MediaType.TEXT_PLAIN_VALUE)
+                    ),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Unauthorized",
+                            content = @Content(mediaType = MediaType.TEXT_PLAIN_VALUE)
+                    ),
+                    @ApiResponse(
+                            responseCode = "403",
+                            description = "Forbidden",
+                            content = @Content(mediaType = MediaType.TEXT_PLAIN_VALUE)
+                    )
+            },
+            tags = TAG_USER_CONTROLLER
+    )
     @PatchMapping("/me")
     public ResponseEntity<?> updateUser(){
         return ResponseEntity.ok("Update user");
     }
 
+    @Operation(
+            summary = "setPassword",
+            description = "newPassword",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE
+//                            schema = @Schema(implementation = NewPassword.class)
+                    )
+            ),
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "OK",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE
+//                                    schema = @Schema(implementation = NewPassword.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "201",
+                            description = "Created",
+                            content = @Content(mediaType = MediaType.TEXT_PLAIN_VALUE)
+                    ),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Unauthorized",
+                            content = @Content(mediaType = MediaType.TEXT_PLAIN_VALUE)
+                    ),
+                    @ApiResponse(
+                            responseCode = "403",
+                            description = "Forbidden",
+                            content = @Content(mediaType = MediaType.TEXT_PLAIN_VALUE)
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Not Found",
+                            content = @Content(mediaType = MediaType.TEXT_PLAIN_VALUE)
+                    )
+            },
+            tags = TAG_USER_CONTROLLER
+    )
     @PostMapping("/set_password")
     public ResponseEntity<?> setPassword(){
         return ResponseEntity.ok("Set password of user");
     }
 
+    @Operation(
+            summary = "getUser",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "OK",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = User.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Unauthorized",
+                            content = @Content(mediaType = MediaType.TEXT_PLAIN_VALUE)
+                    ),
+                    @ApiResponse(
+                            responseCode = "403",
+                            description = "Forbidden",
+                            content = @Content(mediaType = MediaType.TEXT_PLAIN_VALUE)
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Not Found",
+                            content = @Content(mediaType = MediaType.TEXT_PLAIN_VALUE)
+                    )
+            },
+            tags = TAG_USER_CONTROLLER
+    )
     @GetMapping("/{id}")
     public ResponseEntity<?> getUser(@PathVariable Long id){
         return ResponseEntity.ok("Get users with pk = " + id);
