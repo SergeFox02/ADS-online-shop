c0c0371f86f0600f7908012c1a72960252995e72
SergeFox02 <Fox2188@gmail.com>
2022-09-29 14:44:04 +0300

    Написал модель Image.java Для картинок

---
+++ src/main/java/ru/skypro/homework/Model/Image.java
@@ -0,0 +1,94 @@
+package ru.skypro.homework.Model;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+public class Image {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String filePath;
+    private long fileSize;
+    private String mediaType;
+
+    @Lob
+    private byte[] data;
+
+    @OneToOne
+    private Ads ads;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public long getFileSize() {
+        return fileSize;
+    }
+
+    public void setFileSize(long fileSize) {
+        this.fileSize = fileSize;
+    }
+
+    public String getMediaType() {
+        return mediaType;
+    }
+
+    public void setMediaType(String mediaType) {
+        this.mediaType = mediaType;
+    }
+
+    public byte[] getData() {
+        return data;
+    }
+
+    public void setData(byte[] data) {
+        this.data = data;
+    }
+
+    public Ads getAds() {
+        return ads;
+    }
+
+    public void setAds(Ads ads) {
+        this.ads = ads;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Image image = (Image) o;
+        return Objects.equals(id, image.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Image{" +
+                "id=" + id +
+                ", filePath='" + filePath + '\'' +
+                ", fileSize=" + fileSize +
+                ", mediaType='" + mediaType + '\'' +
+                ", ads=" + ads +
+                '}';
+    }
+}
