08e8364425003d9d21d4a8cdb24d8fecdca54bbb
SergeFox02 <Fox2188@gmail.com>
2022-09-29 12:10:39 +0300

    Создал AdsDto.java

---
+++ src/main/java/ru/skypro/homework/dto/AdsDto.java
@@ -0,0 +1,12 @@
+package ru.skypro.homework.dto;
+
+import lombok.Data;
+
+@Data
+public class AdsDto {
+
+    private String title;
+    private String description;
+    private String image;
+    private Float price;
+}
