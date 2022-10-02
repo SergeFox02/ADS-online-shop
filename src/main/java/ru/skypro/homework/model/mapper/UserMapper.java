993d18bb76ca44f64bd7f5183b7909ee316c3a5c
SergeFox02 <Fox2188@gmail.com>
2022-10-02 15:59:09 +0300

    Дописал UserMapper.java

--- src/main/java/ru/skypro/homework/model/mapper/UserMapper.java
+++ src/main/java/ru/skypro/homework/model/mapper/UserMapper.java
@@ -2,6 +2,7 @@ package ru.skypro.homework.model.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import ru.skypro.homework.model.dto.UserDto;
 import ru.skypro.homework.model.entity.User;
 import ru.skypro.homework.model.dto.LoginReq;
 import ru.skypro.homework.model.dto.RegisterReq;
@@ -11,6 +12,8 @@ public interface UserMapper {
 
     @Mapping(source = "username", target = "email")
     User toUser(RegisterReq registerReq);
+
+    User toUser(UserDto userDto);
 
     User loginReqToUserMapping(LoginReq loginReq);
 }
