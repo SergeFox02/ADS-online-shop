eb896fef26895bcb8efc782050a05d9f45ed6062
SergeFox02 <Fox2188@gmail.com>
2022-10-04 16:25:22 +0300

    Сделал AdsMapper через MapStruct

--- src/main/java/ru/skypro/homework/model/mapper/AdsMapper.java
+++ src/main/java/ru/skypro/homework/model/mapper/AdsMapper.java
@@ -1,10 +1,14 @@
 package ru.skypro.homework.model.mapper;
 
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import ru.skypro.homework.model.entity.Ads;
 import ru.skypro.homework.model.dto.AdsDto;
 
+@Mapper(componentModel = "spring")
 public interface AdsMapper {
 
+    @Mapping(source = "author.id", target = "author")
     AdsDto toAdsDto(Ads ads);
 
 }
