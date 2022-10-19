1eb3866ae6dfc5c8d3fe3f6270cedc4edb327342
SergeFox02 <Fox2188@gmail.com>
2022-10-19 13:14:23 +0300

    Написал документацию на AdsCommentsImpl.java

--- src/main/java/ru/skypro/homework/service/impl/AdsCommentsImpl.java
+++ src/main/java/ru/skypro/homework/service/impl/AdsCommentsImpl.java
@@ -17,6 +17,10 @@ import ru.skypro.homework.service.AdsCommentsService;
 
 import java.time.LocalDateTime;
 
+/**
+ * Service for working with AdsComments.
+ */
+
 @Service
 @RequiredArgsConstructor
 public class AdsCommentsImpl implements AdsCommentsService {
@@ -25,6 +29,15 @@ public class AdsCommentsImpl implements AdsCommentsService {
     private final CommentsMapper commentsMapper;
     private final AdsRepository adsRepository;
 
+    /**
+     * Add Comment in Ads with id = ad_pk
+     *
+     * @param ad_pk id of Ads
+     * @param adsComment comment of Ads
+     * @return Adding comment
+     *
+     * @throws NotFoundException if Comment by id = ad_pk not found
+     */
     @Override
     public AdsComment addComment(int ad_pk, AdsComment adsComment) {
         User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
@@ -41,6 +54,15 @@ public class AdsCommentsImpl implements AdsCommentsService {
         return adsComment;
     }
 
+    /**
+     * Delete comment by id, of ads ad_pk
+     *
+     * @param ad_pk id of ads
+     * @param id id of comment
+     * @return removed comment
+     * @throws NotFoundException if ads by ad_pk not found
+     * @throws ResponseStatusException return {@code  HttpStatus.FORBIDDEN} if user is not Admin or not enough rights odf user
+     */
     @Override
     public Comment deleteComment(int ad_pk, int id) throws NotFoundException {
         if (adsRepository.findById(ad_pk).isEmpty() || commentRepository.findById(id).isEmpty()){
@@ -56,6 +78,14 @@ public class AdsCommentsImpl implements AdsCommentsService {
         throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Unavailable to update. It's not your ads!");
     }
 
+    /**
+     * Get comment of Ads
+     *
+     * @param ad_pk ads id
+     * @param id of comment
+     * @return comment by id of ads(ad_pk)
+     * @throws ResponseStatusException {@code HttpStatus.NOT_FOUND} if ads or comment not found
+     */
     @Override
     public AdsComment getComment(int ad_pk, int id) {
         if (adsRepository.findById(ad_pk).isEmpty()){
@@ -68,6 +98,16 @@ public class AdsCommentsImpl implements AdsCommentsService {
         return commentsMapper.toAdsComment(commentRepository.findById(id).get());
     }
 
+    /**
+     * Update ads comment
+     *
+     * @param ad_pk id of ads
+     * @param id comment {@code id}
+     * @param comment update comment of ads
+     * @return update comment
+     * @throws ResponseStatusException {@code HttpStatus.NOT_FOUND} if ads or comment not found
+     * @throws ResponseStatusException return {@code  HttpStatus.FORBIDDEN} if user is not Admin or not enough rights odf user
+     */
     @Override
     public AdsComment updateComment(int ad_pk, int id, AdsComment comment) {
         if (adsRepository.findById(ad_pk).isEmpty()){
